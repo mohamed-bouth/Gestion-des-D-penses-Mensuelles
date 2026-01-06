4e1aff56ee1ef84eee93504dccf90d83e7e667ea
mohamed_bouth <mohamedbouth87@gmail.com>
2026-01-06 14:10:16 +0100

    GDDM-8 create the class of transaction

---
+++ app/models/transaction.php
@@ -0,0 +1,34 @@
+<?php
+
+abstract class transaction {
+    protected $userID;
+    protected $date;
+    protected $montant;
+
+    public function __construct($userID, $date, $montant) {
+        $this->userID = $userID;
+        $this->date = $date;
+        $this->montant = $montant;
+    }
+
+    public function getUserID() {
+        return $this->userID;
+    }
+
+    public function getDate() {
+        return $this->date;
+    }
+
+    public function getMontant() {
+        return $this->montant;
+    }
+
+    public function setdate($date) {
+        $this->date = $date;
+    }
+
+    public function setMontant($montant) {
+        $this->montant = $montant;
+    }
+
+}
