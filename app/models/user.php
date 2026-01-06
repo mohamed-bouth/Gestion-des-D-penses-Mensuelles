ddd17022011787426f4dd6f4c3eedec6f7a8090a
mohamed_bouth <mohamedbouth87@gmail.com>
2026-01-06 14:08:28 +0100

    GDDM-5 create the class of user

---
+++ app/models/user.php
@@ -0,0 +1,32 @@
+<?php 
+
+class user {
+    private $id;
+    private $name;
+    private $email;
+    private $createdAt;
+
+    public function __construct($id , $name , $email , $createdAt) {
+        $this->$id = $id;
+        $this->$name = $name;
+        $this->$email = $email;
+        $this->$createdAt = $createdAt;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setname($name) {
+        $this->name = $name;
+    }
+    public function setemail($email) {
+        $this->name = $email;
+    }
+    public function getname() {
+        return $this->name;
+    }
+    public function getemail() {
+        return $this->email;
+    }
+}
